4e72c9318e3897701680515893888ce1a78e36e4
Rik Bandsma <rik.bandsma@gmail.com>
2024-01-14 16:07:31 +0100

    Set up basics for item route

---
+++ src/app/thing/[code]/page.tsx
@@ -0,0 +1,19 @@
+import { WishlistItem } from '@/types/wishlist'
+import { fetchItem } from '@/data/wishlist/fetchItem'
+
+import classes from './page.module.scss'
+
+export default async function Thing({ params }: { params: { code: string } }) {
+    const wishlistItemResult = await fetchItem(params.code);
+
+    if (Object.hasOwn(wishlistItem, 'error')) {
+        return <h1>{'Oeps'}</h1>
+    }
+    const wishlistItem = wishlistItemResult as WishlistItem;
+    return (
+        <section className={classes.wishlistPage}>
+            <h1>{wishlistItem.name}</h1>
+            <span>{wishlistItem.code}</span>
+        </section>
+    )
+}
---
+++ src/data/wishlist/fetchItem.ts
@@ -0,0 +1,26 @@
+import type { WishlistItem } from '@/types/wishlist'
+import {
+    FETCH_ITEM_BY_CODE,
+} from '@/data/queries/item/get'
+import mapWishlistItem from '@/data/mappers/wishlistItem'
+
+export const fetchItem = async (
+    itemCode,
+): Promise<WishlistItem | {error: boolean}> => {
+    try {
+        const item = await FETCH_ITEM_BY_CODE({itemCode});
+
+        if (item.rowCount === 1) {
+            return mapWishlistItem(item.rows[0])
+        }
+
+        return {
+            'error': true
+        };
+    } catch (e) {
+        console.log(e);
+        return {
+            'error': true,
+        };
+    }
+}
